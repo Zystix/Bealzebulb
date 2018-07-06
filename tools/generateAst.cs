d7f44a2a8317e4bdec45fdb5667f1d9e17dd3f49
Christian <themooresofnott@gmail.com>
2018-07-06 19:44:33 +1000

    worked on tools

--- tools/generateAst.cs
+++ tools/generateAst.cs
@@ -19,21 +19,81 @@ namespace LUCItools
 
       string outputDir = args[0];
 
-      defineAst(outputDir, "Expr", Arrays.asList(
+      defineAst(outputDir, "Expr", new List<string>(){
      "Binary   : Expr left, Token operator, Expr right",
      "Grouping : Expr expression",
      "Literal  : Object value",
      "Unary    : Token operator, Expr right"
-     ));
+     });
     }
 
     private static void defineAst(
       string outputDir, string baseName, List<string> types)
-    )
+
     {
       string path = outputDir + "/" + baseName + ".cs";
-      StreamWriter writer = new StreamWriter(path, "UTF-8");
+      StreamWriter writer = new StreamWriter(path);
 
+      writer.WriteLine("using System;");
+      writer.WriteLine("using System.Collections.Generic;");
+      writer.WriteLine("");
+      writer.WriteLine("namespace LUCI {");
+      writer.WriteLine("");
+      writer.WriteLine("abstract class " + baseName + " {");
+      defineVisitor(writer, baseName, types);
+
+      foreach(string type in types)
+      {
+        string className = type.Split(':')[0].Trim();
+        string fields = type.Split(':')[1].Trim();
+        defineType(writer, baseName, className, fields);
+      }
+      writer.WriteLine("}");
+      writer.Close();
+
+    }
+
+    private static void defineType(
+      StreamWriter writer, string baseName, string className, string fieldList)
+      {
+        writer.WriteLine("  static class " + className + " : " + baseName + " {");
+
+        // Constructor
+        writer.WriteLine("  " + className + "(" + fieldList + ") {");
+
+        // Store parameters in fields.
+        string[] fields = fieldList.Split(',');
+        foreach (string field in fields)
+        {
+          string name = field.Split(' ')[1];
+          writer.WriteLine("    this." + name + " = " + name + ";");
+        }
+
+        writer.WriteLine("    }");
+        // Fields.
+
+        writer.WriteLine();
+        foreach (string field in fields)
+        {
+          writer.WriteLine("    const " + field + ";");
+
+        }
+
+        writer.WriteLine("  }");
+        }
+
+
+    private static void defineVisitor(StreamWriter writer, string baseName, List<string> types)
+    {
+      writer.WriteLine("  interface Visitor<R> {");
+
+      foreach (string type in types)
+      {
+        string typeName = type.Split(':')[0].Trim();
+        writer.WriteLine("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+      }
+
+      writer.println("  }");
     }
   }
 }
